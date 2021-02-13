8c7b13bdb6d5ecbe370102b3accee87e9220913e
NicholasETerry <nicholas.e.terry@gmail.com>
2021-02-13 16:25:04 -0600

    added put functionality to correctly modify database information

--- Client/app.js
+++ Client/app.js
@@ -1,3 +1,5 @@
+let tempId = 0; // stores movieId when handleUpdate function gets called 
+
 //display list of movies in the update section
 $(document).ready(function(){
     $.get("https://localhost:44325/api/movie", function(data){
@@ -8,46 +10,55 @@ $(document).ready(function(){
             <div>Genre: ${JSON.stringify(el.genre)}</div>
             </div>
             <div>
-            <button onclick="handleUpdate(${JSON.stringify(el.movieId)})">Edit</button>
+            <button onclick="handleUpdate(${JSON.stringify(el.movieId)})">Edit</button> 
             
             </div>
             <br>`);
         })
     })
 })
+function findMovie(id){
 
+}
+// handleUpdate is call by button onclick right above this comment.
 function handleUpdate(movieId){
     $.get("https://localhost:44325/api/movie/" +movieId, function(data){
         $("#movieIdEdit").val(data.movieId);
         $("#movieTitleEdit").val(data.title);
         $("#movieDirectorEdit").val(data.director);
         $("#movieGenreEdit").val(data.genre);
+        tempId = movieId;
     })
 }
 
 //function to edit a movie
 
 function updateMovie() {
+    
+    let movieTitle = $("#movieTitleEdit").val();
+    let movieDirector = $("#movieDirectorEdit").val();
+    let movieGenre = $("#movieGenreEdit").val();
     var dict = {
-        MovieId: $("#movieIdEdit").val(),
-        Title: this["title"].value,
-        Genre: this["genre"].value,
-        Director: this["director"].value
+        movieId : tempId,
+        director : movieDirector,
+        title : movieTitle,
+        genre : movieGenre,
     };
 
     $.ajax({
         url: 'https://localhost:44325/api/movie',
-        type: 'PUT',
         dataType: 'json',
+        type: 'put',
         contentType: 'application/json',
         data: JSON.stringify(dict),            
-        success: function(data, textStatus, jqXhr){//success callback function
+        success: function(data, textStatus, jQxhr){//success callback function
             console.log("Updated!!")
         },
-        error: function (jgXhr, textStatus, errorThrown) {
+        error: function (jqXhr, textStatus, errorThrown) {
             console.log(errorThrown);
         }
     });
+    getAfterPut(dict);
 };
 function postMovie(){
     let movieTitle = $("#postTitle").val();
@@ -71,6 +82,7 @@ function postMovie(){
             console.log( errorThrown );
         }
     });
+    
 
             // $.post("https://localhost:44325/api/movie",JSON.stringify(dict), function(data){
             //     $("#movieDetails").append(`<div>
@@ -80,8 +92,22 @@ function postMovie(){
             //         </div><br>`);
             //     })
                    
-}
-
+};
+function getAfterPut(dictonary){
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        type: 'get',
+        contentType: 'application/json',
+        data: JSON.stringify(dictonary),            
+        success: function(data, textStatus, jQxhr){//success callback function
+            console.log("get after posting to database")
+        },
+        error: function (jqXhr, textStatus, errorThrown) {
+            console.log(errorThrown);
+        }
+    });
+};
 //     $("#sendGet").click(function(e){
 //         e.preventDefault();
 //         $(document).ready(function(){
