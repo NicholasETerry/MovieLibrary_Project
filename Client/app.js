11669f8d084a24e271eec47e74658e1f834bb936
NicholasETerry <nicholas.e.terry@gmail.com>
2021-02-13 22:05:47 -0600

    fix issues with function processForm opening and closing

--- Client/app.js
+++ Client/app.js
@@ -1,157 +1,121 @@
-//display list of movies in a table inside the movie detail section
-$(document).ready(function(){
-    $.get("https://localhost:44325/api/movie", function(data){
-        var movie = '';
-        $.each(data, function(){
-            movie += '<tr>';
-            movie += '<td>'+this.title+'</td>';
-            movie += '<td>'+this.director+'</td>';
-            movie += '<td>'+this.genre+'</td>';
-            movie += '</tr>';
+ (function($){
+     function processForm(e){
+     }
+ 
+    //display list of movies in a table inside the movie detail section
+        $(document).ready(function(){
+            $.get("https://localhost:44325/api/movie", function(data){
+                var movie = '';
+                $.each(data, function(){
+                    movie += '<tr>';
+                    movie += '<td>'+this.title+'</td>';
+                    movie += '<td>'+this.director+'</td>';
+                    movie += '<td>'+this.genre+'</td>';
+                    movie += '</tr>';
+                })
+                $('#tableBody').append(movie);
+                })
+            })
+
+        //display list of movies in the update section
+        $(document).ready(function(){
+            $.get("https://localhost:44325/api/movie", function(data){
+                data.map(function(el){
+                    $("#listOfMoviesToUpdate").append(`<div>
+                    <div>Title: ${JSON.stringify(el.title)}</div>
+                    <div>Director: ${JSON.stringify(el.director)}</div>
+                    <div>Genre: ${JSON.stringify(el.genre)}</div>
+                    </div>
+                    <div>
+                    <button type="button" class="btn btn-primary btn-sm" onclick="handleUpdate(${JSON.stringify(el.movieId)})">Edit</button>            
+                    </div>            
+                    <br>`);            
+                })        
+            })
         })
-        $('#tableBody').append(movie);
-        })
-    })
-
-//display list of movies in the update section
-$(document).ready(function(){
-    $.get("https://localhost:44325/api/movie", function(data){
-        data.map(function(el){
-            $("#listOfMoviesToUpdate").append(`<div>
-            <div>Title: ${JSON.stringify(el.title)}</div>
-            <div>Director: ${JSON.stringify(el.director)}</div>
-            <div>Genre: ${JSON.stringify(el.genre)}</div>
-            </div>
-            <div>
-            <button type="button" class="btn btn-primary btn-sm" onclick="handleUpdate(${JSON.stringify(el.movieId)})">Edit</button>            
-            </div>            
-            <br>`);            
-        })        
-    })
-})
-// handleUpdate is call by button onclick right above this comment.
-function handleUpdate(movieId){
-    $.get("https://localhost:44325/api/movie/" +movieId, function(data){
-        $("#movieIdEdit").val(data.movieId);
-        $("#movieTitleEdit").val(data.title);
-        $("#movieDirectorEdit").val(data.director);
-        $("#movieGenreEdit").val(data.genre);
-        tempId = movieId;
-    })
-}
-
-//function to edit a movie
-
-function updateMovie() {
-    
-    let movieTitle = $("#movieTitleEdit").val();
-    let movieDirector = $("#movieDirectorEdit").val();
-    let movieGenre = $("#movieGenreEdit").val();
-    var dict = {
-        movieId : tempId,
-        director : movieDirector,
-        title : movieTitle,
-        genre : movieGenre,
-    };
-
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        dataType: 'json',
-        type: 'put',
-        contentType: 'application/json',
-        data: JSON.stringify(dict),            
-        success: function(data, textStatus, jQxhr){//success callback function
-            console.log("Updated!!");
-            
-        },
-        error: function (jqXhr, textStatus, errorThrown) {
-            console.log(errorThrown);
+        // handleUpdate is call by button onclick right above this comment.
+        function handleUpdate(movieId){
+            $.get("https://localhost:44325/api/movie/" +movieId, function(data){
+                $("#movieIdEdit").val(data.movieId);
+                $("#movieTitleEdit").val(data.title);
+                $("#movieDirectorEdit").val(data.director);
+                $("#movieGenreEdit").val(data.genre);
+                tempId = movieId;
+            })
         }
-    });
-    getAfterPut(dict);
-};
-function postMovie(){
-    let movieTitle = $("#postTitle").val();
-    let movieDirector =$("#postDirector").val();
-    let movieGenre = $("#postGenre").val();
-    let dict = {
-        director: movieDirector,
-        title :movieTitle,
-        genre: movieGenre
-    }
-    $.ajax({
-        url: 'https://localhost:44325/api/movie',
-        dataType: 'json',
-        type: 'post',
-        contentType: 'application/json',
-        data: JSON.stringify(dict),
-        success: function( data, textStatus, jQxhr ){
-            console.log("post successful. check database for correct information.");
-        },
-        error: function( jqXhr, textStatus, errorThrown ){
-            console.log( errorThrown );
-        }
-    });
-    
 
-            // $.post("https://localhost:44325/api/movie",JSON.stringify(dict), function(data){
-            //     $("#movieDetails").append(`<div>
-            //     <div> Title: ${JSON.stringify(data.title)}</div>
-            //     <div> Title: ${JSON.stringify(data.director)}</div>
-            //         <div> Title: ${JSON.stringify(data.genre)}</div>
-            //         </div><br>`);
-            //     })
-                   
-};
-function getAfterPut(dictonary){
-    // $.ajax({
-    //     url: 'https://localhost:44325/api/movie',
-    //     dataType: 'json',
-    //     type: 'get',
-    //     contentType: 'application/json',
-    //     data: JSON.stringify(dictonary),            
-    //     success: function(data, textStatus, jQxhr){//success callback function
-    //         console.log("get after posting to database")
-    //     },
-    //     error: function (jqXhr, textStatus, errorThrown) {
-    //         console.log(errorThrown);
-    //     }
-    // });
-    $.get("https://localhost:44325/api/movie", function(data){
-        data.map(function(el){
-            $("#updateMovies").append(`<div>
-            <div>Title: ${JSON.stringify(el.title)}</div>
-            <div>Director: ${JSON.stringify(el.director)}</div>
-            <div>Genre: ${JSON.stringify(el.genre)}</div>
-            </div>
-            <div>
-            <button onclick="handleUpdate(${JSON.stringify(el.movieId)})">Edit</button> 
+        //function to edit a movie
+
+        function updateMovie() {
             
-            </div>
-            <br>`);
-        })
-    })
-};
-//     $("#sendGet").click(function(e){
-//         e.preventDefault();
-//         $(document).ready(function(){
-//             $.get("https://localhost:44325/api/movie", function(data){
-//                 for(let i = 0; i < data.length; i++){
-//                     $("movieDetails").append(`<div>
-//                     <div>Title: ${JSON.stringify(data[i].title)}</div>
-//                     <div>Director: ${JSON.stringify(data[i].director)}</div>
-//                     <div>Genre: ${JSON.stringify(data[i].genre)}</div>
-//                     </div><br>`);
-//                 }
+            let movieTitle = $("#movieTitleEdit").val();
+            let movieDirector = $("#movieDirectorEdit").val();
+            let movieGenre = $("#movieGenreEdit").val();
+            var dict = {
+                movieId : tempId,
+                director : movieDirector,
+                title : movieTitle,
+                genre : movieGenre,
+            };
 
-//             })
-//         })
-//     })
-//     $('#my-form').submit( processForm );
-// })(jQuery);
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: 'json',
+                type: 'put',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),            
+                success: function(data, textStatus, jQxhr){//success callback function
+                    console.log("Updated!!");
+                    
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    console.log(errorThrown);
+                }
+            });
+            getAfterPut(dict);
+        };
+        function postMovie(){
+            let movieTitle = $("#postTitle").val();
+            let movieDirector =$("#postDirector").val();
+            let movieGenre = $("#postGenre").val();
+            let dict = {
+                director: movieDirector,
+                title :movieTitle,
+                genre: movieGenre
+            }
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function( data, textStatus, jQxhr ){
+                    console.log("post successful. check database for correct information.");
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+                        
+        };
+        function getAfterPut(dictonary){
 
-// //Update the details of a movie, including title, genre, and director name
-// (function (){
-//     function updateMovie(currentMovie){
-        
-//     }
+            $.get("https://localhost:44325/api/movie", function(data){
+                data.map(function(el){
+                    $("#updateMovies").append(`<div>
+                    <div>Title: ${JSON.stringify(el.title)}</div>
+                    <div>Director: ${JSON.stringify(el.director)}</div>
+                    <div>Genre: ${JSON.stringify(el.genre)}</div>
+                    </div>
+                    <div>
+                    <button onclick="handleUpdate(${JSON.stringify(el.movieId)})">Edit</button> 
+                    
+                    </div>
+                    <br>`);
+                })
+            })
+        };
+
+    $('#my-form').submit( processForm );
+})(jQuery);
+
